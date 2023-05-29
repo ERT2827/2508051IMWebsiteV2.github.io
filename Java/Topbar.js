18a2d185d96ec20c3e6a1d3bbe23ae7e0fe8e115
ERT2827 <Btonkin5@gmail.com>
2023-05-29 11:56:23 +0200

    The whole website
    This is pretty much the entire website prototype, to be cleaned up once I actually finalize the whole thing in a little bit.

---
+++ Java/Topbar.js
@@ -0,0 +1,50 @@
+const pageLinks = [{
+    id: 1,
+    location: "index.html",
+    text: "Home"
+},{
+    id: 2,
+    location: "portfolio.html",
+    text: "Portfolio"
+},{
+    id: 3,
+    location: "blogs.html",
+    text: "Blog"   
+},{
+    id: 4,
+    location: "essays.html",
+    text: "Essays"   
+},{
+    id: 5,
+    location: "design.html",
+    text: "Design"   
+},{
+    id: 6,
+    location: "profile.html",
+    text: "Profile"   
+}]
+
+
+const topBarArea = document.querySelector(".nav-area");
+
+displayTopBar(pageLinks);
+
+function displayTopBar(pageses){
+    let displayTopBar = pageses.map(function (pageses){
+        return `<li><a href="${pageses.location}">${pageses.text}</a></li>`;
+      }).join(" ");
+
+    topBarArea.innerHTML = displayTopBar;
+}
+
+// Back to top functionality
+
+const toTop = document.querySelector(".to-Top");
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 100){
+        toTop.classList.add("active");
+    }else{
+        toTop.classList.remove("active");
+    }
+})
